acb5b84b79c43fe7a4913ec96d4542285da91796
Miguel Solís La Beira <miguel.solis.lb@gmail.com>
2018-10-10 12:52:28 +0200

    Add files via upload

---
+++ Tema5/Tema5Ejercicio5.java
@@ -0,0 +1,11 @@
+public class Tema5Ejercicio5 {
+public static void main(String[] args) {
+	
+	int numero = 320;
+	
+	while (numero >=160) {
+		System.out.println(numero);
+		numero = numero - 20;
+	}
+ }
+}	
