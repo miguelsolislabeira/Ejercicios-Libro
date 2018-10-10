1adf1ef2395cab145920d788a62b914c355bf136
Miguel Solís La Beira <miguel.solis.lb@gmail.com>
2018-10-10 11:58:22 +0200

    Add files via upload

---
+++ Tema5Ejercicio3.java
@@ -0,0 +1,15 @@
+public class Tema5Ejercicio3 {
+public static void main(String [] args) {
+
+//Muestra los números múltiplos del 5 del 0 al 100
+
+	int i = 0;
+	
+	do {
+		System.out.println(i);
+		i=i + 5;
+	} while
+		(i <= 100);
+		
+ }
+}
---
+++ Tema5Ejercicio4.java
@@ -0,0 +1,10 @@
+public class Tema5Ejercicio4 {
+	public static void main(String[] args) {
+		
+		int num;
+		for ( num = 320; num >=160; num = num -20) {
+			System.out.println(num);
+		}
+		
+	}
+}
